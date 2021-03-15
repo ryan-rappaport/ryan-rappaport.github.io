97d19478468fd575239a5443684741252a2c822a
Ryan Rappaport <rappaportry17@outlook.com>
2021-03-15 17:25:13 -0400

    Changed .close to .closeimg

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,7 +399,7 @@
 			}
 
 			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close")[0];
+			var span = document.getElementsByClassName("closeimg")[0];
 
 			// When the user clicks on <span> (x), close the modal
 			span.onclick = function() {
